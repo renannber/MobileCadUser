e3c09dee40048895d2a712d63a18b73934c7dfd9
renan berr <renannber.ads@gmail.com>
2026-03-19 21:59:32 -0300

    Implementação dos métodos para vincular variavel, função de click e converção String

--- app/src/main/java/com/nan/cadastrousuario/CreateUser.java
+++ app/src/main/java/com/nan/cadastrousuario/CreateUser.java
@@ -1,4 +1,39 @@
 package com.nan.cadastrousuario;
+//Bloco de importações
+ import android.os.Bundle;
+ import android.widget.Button;
 
-public class CreateUser {
+ import androidx.appcompat.app.AppCompatActivity;
+ import com.google.android.material.textfield.TextInputEditText;
+
+//Classe principal
+public class CreateUser extends AppCompatActivity {
+    TextInputEditText editNome, editEmail;
+
+    Button btnSalvar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+         // vinculo variavel com o elemento da interface grafica, escrito no arquivo xml
+        editNome = findViewById(R.id.nome);
+        editEmail = findViewById(R.id.email);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+       //função de click
+        btnSalvar.setOnClickListener(v -> {
+
+        //pega o que for digitado no input nome e email e converte para String
+        String nome = editNome.getText().toString();
+        String email = editEmail.getText().toString();
+
+        if(!nome.isEmpty()){
+            MainActivity.listNomes.add(nome + " (" + email + ") ");
+            finish();
+
+        }
+    });
+    }
+
 }
--- app/src/main/java/com/nan/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/nan/cadastrousuario/UserAdapter.java
@@ -1,12 +1,1 @@
-package com.nan.cadastrousuario;
-//Bloco de importações
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.textfield.TextInputEditText;
 
-//Classe principal
-public class UserAdapter extends AppCompatActivity {
-    TextInputEditText editNome, editEmail;
-
-    Button btnSalvar;
-
-}
